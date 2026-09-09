933fa8d065c5351024d0acc8b5aceb5943aa9897
Richmond Adu-Kyere <adukyerer@gmail.com>
2026-09-09 17:40:16 +0000

    style: redesign Tekora premium light header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -2,15 +2,26 @@ import Link from "next/link";
 
 export function SiteHeader() {
   return (
-    <header className="siteHeader">
-      <Link href="/" className="brandMark">TEKORA</Link>
-      <nav className="siteNav">
-        <Link href="#learn">Learn</Link>
-        <Link href="#build">Build</Link>
-        <Link href="#connect">Connect</Link>
-        <Link href="/sign-in">Sign in</Link>
-        <Link href="/onboarding" className="headerCta">Join Tekora</Link>
-      </nav>
+    <header className="premiumHeader">
+      <div className="premiumHeaderInner">
+        <Link href="/" className="premiumBrand" aria-label="Tekora home">
+          <span className="premiumBrandIcon">T</span>
+          <span>Tekora</span>
+        </Link>
+
+        <nav className="premiumNav" aria-label="Primary navigation">
+          <Link href="/learn">Learn</Link>
+          <Link href="/creator/courses">Teach</Link>
+          <Link href="#community">Community</Link>
+          <Link href="#industry">For Industry</Link>
+          <Link href="#platform">Platform</Link>
+        </nav>
+
+        <div className="premiumHeaderActions">
+          <Link href="/sign-in" className="premiumSignIn">Sign in</Link>
+          <Link href="/onboarding" className="premiumGetStarted">Get started</Link>
+        </div>
+      </div>
     </header>
   );
 }
